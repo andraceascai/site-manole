15357ec91427d1d0fb7929c56280db9c0cca6539
Ceascai Andra Maria <maceascai@gmail.com>
2025-12-22 14:14:49 +0200

    migration from cloudinary to cloudflare

--- site-manole-main/src/data/mockData.ts
+++ site-manole-main/src/data/mockData.ts
@@ -7,32 +7,32 @@ export interface PortfolioPhoto {
 export const portfolioPhotos: PortfolioPhoto[] = [
   {
     id: '1',
-    url: 'https://res.cloudinary.com/dzpbhkapo/image/upload/v1764849491/boook_akbtmt.png',
+    url: 'https://imagedelivery.net/KQi4tCzGZRQmaudEvugp8w/c9ab4cb1-51f9-48fa-f389-cecfb33ab200/public',
     alt: 'Professional headshot'
   },
   {
     id: '2',
-    url: 'https://res.cloudinary.com/dzpbhkapo/image/upload/v1763641123/Untitled_Session09963_gtkqsc.jpg',
+    url: 'https://imagedelivery.net/KQi4tCzGZRQmaudEvugp8w/024f1f00-1b6f-45ad-57d8-53877fc9ed00/public',
     alt: 'Professional headshot'
   },
   {
     id: '3',
-    url: 'https://res.cloudinary.com/dzpbhkapo/image/upload/v1763641129/3BAR0011_of0ks3.jpg',
+    url: 'https://imagedelivery.net/KQi4tCzGZRQmaudEvugp8w/6cca37a1-4709-4c6d-c578-7d891cc9f200/public',
     alt: 'Stage performance moment'
   },
   {
     id: '4',
-    url: 'https://res.cloudinary.com/dzpbhkapo/image/upload/v1763641123/Untitled_Session09943_xnd5bk.jpg',
+    url: 'https://imagedelivery.net/KQi4tCzGZRQmaudEvugp8w/854a80ba-6808-4dae-7d29-250d7e89d800/public',
     alt: 'Character portrayal'
   },
   {
     id: '5',
-    url: 'https://res.cloudinary.com/dzpbhkapo/image/upload/v1763641124/3BAR0044_m8p1pz.jpg',
+    url: 'https://imagedelivery.net/KQi4tCzGZRQmaudEvugp8w/6e9d540e-e650-49cc-f1a7-01083f4ae200/public',
     alt: 'Theatrical expression'
   },
   {
     id: '6',
-    url: 'https://res.cloudinary.com/dzpbhkapo/image/upload/v1763641122/Untitled_Session09927_ptqhla.jpg',
+    url: 'https://imagedelivery.net/KQi4tCzGZRQmaudEvugp8w/d5580637-4bab-4024-cda5-55a3224a3100/public',
     alt: 'Dramatic scene'
   },
 ];
